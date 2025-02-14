8938205ec40ab706ff5eebdec4c9d641c5efaad7
younasrajput <43384718+younasrajput@users.noreply.github.com>
2025-02-14 10:28:27 -0800

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MatrixEffect from '../components/MatrixEffect';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1 className="neon-text">Welcome to PakNodes Army</h1>
+      <p className="neon-text">Your gateway to decentralized validation.</p>
+      <MatrixEffect />
+    </div>
+  );
+};
+
+export default Home;
