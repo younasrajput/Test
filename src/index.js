9283eed6f11cb1c179e9d9ea6633009e024f9527
younasrajput <43384718+younasrajput@users.noreply.github.com>
2025-02-14 10:30:49 -0800

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './assets/styles/global.css';
+import './assets/styles/theme.css';
+import './assets/styles/animations.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
