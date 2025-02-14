000bd7848e333b19a2f7fb389bb5a247425b3153
younasrajput <43384718+younasrajput@users.noreply.github.com>
2025-02-14 10:29:47 -0800

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/styles/theme.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p className="neon-text">&copy; 2023 PakNodes Army. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
