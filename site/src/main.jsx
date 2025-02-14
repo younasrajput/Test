7955ac21adbdc8c34b887a918bd31928ad5ad7d4
younasrajput <43384718+younasrajput@users.noreply.github.com>
2025-02-14 10:03:01 -0800

    Create main.jsx

---
+++ site/src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './assets/styles/global.css';
+import './assets/styles/theme.css';
+import './assets/styles/animations.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
