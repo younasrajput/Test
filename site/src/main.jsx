7cd54bf3fb47ec60fb9a1cc9100e325fa245b13d
younasrajput <43384718+younasrajput@users.noreply.github.com>
2025-02-14 10:17:41 -0800

    Delete site directory

--- site/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './assets/styles/global.css';
-import './assets/styles/theme.css';
-import './assets/styles/animations.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
